db42b984079429462ab33bcd7d1155185f573448
Chia-Chun Wu <ccwu0918@gmail.com>
2026-01-18 01:40:03 +0800

    Update page.tsx

--- app/(root)/watchlist/page.tsx
+++ app/(root)/watchlist/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Star } from 'lucide-react';
 import { searchStocks } from '@/lib/actions/finnhub.actions';
 import SearchCommand from '@/components/SearchCommand';
